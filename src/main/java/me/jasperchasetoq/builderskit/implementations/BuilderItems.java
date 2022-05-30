1eb945ecc3fea68b558294b7ff518f4ccc94e2d4
TerslenK <88115382+TerslenK@users.noreply.github.com>
2022-05-30 20:38:58 +0300

    Update BuilderItems.java

--- src/main/java/me/jasperchasetoq/builderskit/implementations/BuilderItems.java
+++ src/main/java/me/jasperchasetoq/builderskit/implementations/BuilderItems.java
@@ -9,71 +9,57 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class BuilderItems {
+    //Items gets created here
     public static SlimefunItemStack BB_DEEPSLATE_BRICKS = new SlimefunItemStack("BB_DEEPSLATE_BRICK", Material.DEEPSLATE_BRICKS, "&aBB Deepslate Bricks", "", "&7Builders block");
+    public static SlimefunItemStack BB_DIAMOND_BLOCK = new SlimefunItemStack("BB_DIAMOND_BLOCK", Material.DIAMOND_BLOCK, "&aBB Deepslate Bricks", "", "&7Builders block");
+    public static SlimefunItemStack BB_STONE_BRICK = new SlimefunItemStack("BB_STONE_BRICK", Material.STONE_BRICKS, "&aBB Stone Brick", "", "&7Builders block");
+    public static SlimefunItemStack BB_GLOWSTONE = new SlimefunItemStack("BB_GLOWSTONE", Material.GLOWSTONE, "&aBB Glowstone", "", "&7Builders block");
+    public static SlimefunItemStack BB_DEEPSLATE_BRICK_SLAB = new SlimefunItemStack("BB_DEEPSLATE_BRICK_SLAB", Material.DEEPSLATE_BRICK_SLAB, "&aBB Deepslate Brick Slab", "", "&7Builders block");
+    public static SlimefunItemStack BB_DEEPSLATE_BRICK_STAIRS = new SlimefunItemStack("BB_DEEPSLATE_BRICK_STAIRS", Material.DEEPSLATE_BRICK_STAIRS, "&aBB Deepslate Brick Stairs", "", "&7Builders block");
+    public static SlimefunItemStack BB_DEEPSLATE_BRICK_WALL = new SlimefunItemStack("BB_DEEPSLATE_BRICK_WALL", Material.DEEPSLATE_BRICK_WALL, "&aBB Deepslate Brick Wall", "", "&7Builders block");
 
+    //And they get registered here
     public static void setup(BuildersKit bk) {
         new BuildersBlock(Category.MAIN, BB_DEEPSLATE_BRICKS, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                 new ItemStack(Material.DEEPSLATE_BRICKS),    new ItemStack(Material.DEEPSLATE_BRICKS),                              new ItemStack(Material.DEEPSLATE_BRICKS),
                 new ItemStack(Material.DEEPSLATE_BRICKS),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.DEEPSLATE_BRICKS),
                 new ItemStack(Material.DEEPSLATE_BRICKS),    new ItemStack(Material.DEEPSLATE_BRICKS),                              new ItemStack(Material.DEEPSLATE_BRICKS),
         }).register(bk);
-    }
-    public static SlimefunItemStack BB_DIAMOND_BLOCK = new SlimefunItemStack("BB_DIAMOND_BLOCK", Material.DIAMOND_BLOCK, "&aBB Deepslate Bricks", "", "&7Builders block");
 
-    public static void setup(BuildersKit bk) {
         new BuildersBlock(Category.MAIN, BB_DIAMOND_BLOCK, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                 new ItemStack(Material.DIAMOND_BLOCK),    new ItemStack(Material.DIAMOND_BLOCK),                              new ItemStack(Material.DIAMOND_BLOCK),
                 new ItemStack(Material.DIAMOND_BLOCK),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.DIAMOND_BLOCK),
                 new ItemStack(Material.DIAMOND_BLOCK),    new ItemStack(Material.DIAMOND_BLOCK),                              new ItemStack(Material.DIAMOND_BLOCK),
         }).register(bk);
-    }
 
-}
-    public static SlimefunItemStack BB_STONE_BRICK = new SlimefunItemStack("BB_STONE_BRICK", Material.STONE_BRICKS, "&aBB Stone Brick", "", "&7Builders block");
-
-    public static void setup(BuildersKit bk) {
         new BuildersBlock(Category.MAIN, BB_STONE_BRICK, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                 new ItemStack(Material.STONE_BRICKS),    new ItemStack(Material.STONE_BRICKS),                              new ItemStack(Material.STONE_BRICKS),
                 new ItemStack(Material.STONE_BRICKS),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.STONE_BRICKS),
                 new ItemStack(Material.STONE_BRICKS),    new ItemStack(Material.STONE_BRICKS),                              new ItemStack(Material.STONE_BRICKS),
         }).register(bk);
-    }
-    public static SlimefunItemStack BB_GLOWSTONE = new SlimefunItemStack("BB_GLOWSTONE", Material.GLOWSTONE, "&aBB Glowstone", "", "&7Builders block");
 
-    public static void setup(BuildersKit bk) {
         new BuildersBlock(Category.MAIN, BB_GLOWSTONE, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                 new ItemStack(Material.GLOWSTONE),    new ItemStack(Material.GLOWSTONE),                              new ItemStack(Material.GLOWSTONE),
                 new ItemStack(Material.GLOWSTONE),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.GLOWSTONE),
                 new ItemStack(Material.GLOWSTONE),    new ItemStack(Material.GLOWSTONE),                              new ItemStack(Material.GLOWSTONE),
         }).register(bk);
-    }
 
-    public static SlimefunItemStack BB_DEEPSLATE_BRICK_SLAB = new SlimefunItemStack("BB_DEEPSLATE_BRICK_SLAB", Material.DEEPSLATE_BRICK_SLAB, "&aBB Deepslate Brick Slab", "", "&7Builders block");
-
-    public static void setup(BuildersKit bk) {
         new BuildersBlock(Category.MAIN, BB_DEEPSLATE_BRICK_SLAB, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                 new ItemStack(Material.DEEPSLATE_BRICK_SLAB),    new ItemStack(Material.DEEPSLATE_BRICK_SLAB),                              new ItemStack(Material.DEEPSLATE_BRICK_SLAB),
                 new ItemStack(Material.DEEPSLATE_BRICK_SLAB),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.DEEPSLATE_BRICK_SLAB),
                 new ItemStack(Material.DEEPSLATE_BRICK_SLAB),    new ItemStack(Material.DEEPSLATE_BRICK_SLAB),                              new ItemStack(Material.DEEPSLATE_BRICK_SLAB),
         }).register(bk);
-    }
-    public static SlimefunItemStack BB_DEEPSLATE_BRICK_STAIRS = new SlimefunItemStack("BB_DEEPSLATE_BRICK_STAIRS", Material.DEEPSLATE_BRICK_STAIRS, "&aBB Deepslate Brick Stairs", "", "&7Builders block");
 
-    public static void setup(BuildersKit bk) {
         new BuildersBlock(Category.MAIN, BB_DEEPSLATE_BRICK_STAIRS, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                 new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),    new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),                              new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),
                 new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),
                 new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),    new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),                              new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),
         }).register(bk);
 
-        public static SlimefunItemStack BB_DEEPSLATE_BRICK_WALL = new SlimefunItemStack("BB_DEEPSLATE_BRICK_WALL", Material.DEEPSLATE_BRICK_WALL, "&aBB Deepslate Brick Wall", "", "&7Builders block");
-
-        public static void setup(BuildersKit bk) {
-            new BuildersBlock(Category.MAIN, BB_DEEPSLATE_BRICK_WALL, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
+        new BuildersBlock(Category.MAIN, BB_DEEPSLATE_BRICK_WALL, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                     new ItemStack(Material.DEEPSLATE_BRICK_WALL),    new ItemStack(Material.DEEPSLATE_BRICK_WALL),                              new ItemStack(Material.DEEPSLATE_BRICK_WALL),
                     new ItemStack(Material.DEEPSLATE_BRICK_WALL),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.DEEPSLATE_BRICK_WALL),
                     new ItemStack(Material.DEEPSLATE_BRICK_WALL),    new ItemStack(Material.DEEPSLATE_BRICK_WALL),                              new ItemStack(Material.DEEPSLATE_BRICK_WALL),
-            }).register(bk);
-        }
+        }).register(bk);
     }
 }
