525f16f3e1ea2d60b59116aaad0d2f0ff2449315
terslenk <terslenkhd@gmail.com>
2022-05-31 22:02:26 +0300

    haha multiple output goes brrr

--- src/main/java/me/jasperchasetoq/builderskit/implementations/classes/BuildersBlock/BuildersBlock.java
+++ src/main/java/me/jasperchasetoq/builderskit/implementations/classes/BuildersBlock/BuildersBlock.java
@@ -6,12 +6,18 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockUseHandler;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import org.bukkit.inventory.ItemStack;
 
 public class BuildersBlock extends SlimefunItem {
-
+    //This is for single output
     public BuildersBlock(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
+    }
+
+    //This is for multiple amount of same output
+    public BuildersBlock(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe, CustomItemStack output) {
+        super(itemGroup, item, recipeType, recipe, output);
     }
 
     @Override
--- src/main/java/me/jasperchasetoq/builderskit/implementations/items/BuilderItems.java
+++ src/main/java/me/jasperchasetoq/builderskit/implementations/items/BuilderItems.java
@@ -2,6 +2,7 @@ package me.jasperchasetoq.builderskit.implementations.items;
 
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import me.jasperchasetoq.builderskit.BuildersKit;
 import me.jasperchasetoq.builderskit.implementations.classes.BuildersBlock.BuildersBlock;
 import me.jasperchasetoq.builderskit.utils.Category;
@@ -9,7 +10,7 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class BuilderItems {
-    //Items gets created here
+    //Items get created here
     public static SlimefunItemStack BB_DEEPSLATE_BRICKS = new SlimefunItemStack("BB_DEEPSLATE_BRICK", Material.DEEPSLATE_BRICKS, "&aBB Deepslate Bricks", "", "&7Builders block");
     public static SlimefunItemStack BB_DIAMOND_BLOCK = new SlimefunItemStack("BB_DIAMOND_BLOCK", Material.DIAMOND_BLOCK, "&aBB Deepslate Bricks", "", "&7Builders block");
     public static SlimefunItemStack BB_STONE_BRICK = new SlimefunItemStack("BB_STONE_BRICK", Material.STONE_BRICKS, "&aBB Stone Brick", "", "&7Builders block");
@@ -24,42 +25,42 @@ public class BuilderItems {
                 new ItemStack(Material.DEEPSLATE_BRICKS),    new ItemStack(Material.DEEPSLATE_BRICKS),                              new ItemStack(Material.DEEPSLATE_BRICKS),
                 new ItemStack(Material.DEEPSLATE_BRICKS),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.DEEPSLATE_BRICKS),
                 new ItemStack(Material.DEEPSLATE_BRICKS),    new ItemStack(Material.DEEPSLATE_BRICKS),                              new ItemStack(Material.DEEPSLATE_BRICKS),
-        }).register(bk);
+        },new CustomItemStack(BB_DEEPSLATE_BRICKS, 16)).register(bk);
 
         new BuildersBlock(Category.MAIN, BB_DIAMOND_BLOCK, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                 new ItemStack(Material.DIAMOND_BLOCK),    new ItemStack(Material.DIAMOND_BLOCK),                              new ItemStack(Material.DIAMOND_BLOCK),
                 new ItemStack(Material.DIAMOND_BLOCK),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.DIAMOND_BLOCK),
                 new ItemStack(Material.DIAMOND_BLOCK),    new ItemStack(Material.DIAMOND_BLOCK),                              new ItemStack(Material.DIAMOND_BLOCK),
-        }).register(bk);
+        },new CustomItemStack(BB_DIAMOND_BLOCK, 16)).register(bk);
 
         new BuildersBlock(Category.MAIN, BB_STONE_BRICK, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                 new ItemStack(Material.STONE_BRICKS),    new ItemStack(Material.STONE_BRICKS),                              new ItemStack(Material.STONE_BRICKS),
                 new ItemStack(Material.STONE_BRICKS),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.STONE_BRICKS),
                 new ItemStack(Material.STONE_BRICKS),    new ItemStack(Material.STONE_BRICKS),                              new ItemStack(Material.STONE_BRICKS),
-        }).register(bk);
+        },new CustomItemStack(BB_STONE_BRICK, 16)).register(bk);
 
         new BuildersBlock(Category.MAIN, BB_GLOWSTONE, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                 new ItemStack(Material.GLOWSTONE),    new ItemStack(Material.GLOWSTONE),                              new ItemStack(Material.GLOWSTONE),
                 new ItemStack(Material.GLOWSTONE),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.GLOWSTONE),
                 new ItemStack(Material.GLOWSTONE),    new ItemStack(Material.GLOWSTONE),                              new ItemStack(Material.GLOWSTONE),
-        }).register(bk);
+        },new CustomItemStack(BB_GLOWSTONE, 16)).register(bk);
 
         new BuildersBlock(Category.MAIN, BB_DEEPSLATE_BRICK_SLAB, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                 new ItemStack(Material.DEEPSLATE_BRICK_SLAB),    new ItemStack(Material.DEEPSLATE_BRICK_SLAB),                              new ItemStack(Material.DEEPSLATE_BRICK_SLAB),
                 new ItemStack(Material.DEEPSLATE_BRICK_SLAB),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.DEEPSLATE_BRICK_SLAB),
                 new ItemStack(Material.DEEPSLATE_BRICK_SLAB),    new ItemStack(Material.DEEPSLATE_BRICK_SLAB),                              new ItemStack(Material.DEEPSLATE_BRICK_SLAB),
-        }).register(bk);
+        },new CustomItemStack(BB_DEEPSLATE_BRICK_SLAB, 16)).register(bk);
 
         new BuildersBlock(Category.MAIN, BB_DEEPSLATE_BRICK_STAIRS, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                 new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),    new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),                              new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),
                 new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),
                 new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),    new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),                              new ItemStack(Material.DEEPSLATE_BRICK_STAIRS),
-        }).register(bk);
+        },new CustomItemStack(BB_DEEPSLATE_BRICK_STAIRS)).register(bk);
 
         new BuildersBlock(Category.MAIN, BB_DEEPSLATE_BRICK_WALL, RecipeType.ENHANCED_CRAFTING_TABLE,new ItemStack[] {
                     new ItemStack(Material.DEEPSLATE_BRICK_WALL),    new ItemStack(Material.DEEPSLATE_BRICK_WALL),                              new ItemStack(Material.DEEPSLATE_BRICK_WALL),
                     new ItemStack(Material.DEEPSLATE_BRICK_WALL),    new ItemStack(Material.NETHER_STAR),                              new ItemStack(Material.DEEPSLATE_BRICK_WALL),
                     new ItemStack(Material.DEEPSLATE_BRICK_WALL),    new ItemStack(Material.DEEPSLATE_BRICK_WALL),                              new ItemStack(Material.DEEPSLATE_BRICK_WALL),
-        }).register(bk);
+        },new CustomItemStack(BB_DEEPSLATE_BRICK_WALL, 16)).register(bk);
     }
 }
